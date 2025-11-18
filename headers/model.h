a05ef83005cf8c794aac1f3e8fa294d74c3f173f
Rouf0x <gabrielruf.fr@gmail.com>
2025-11-18 19:09:32 +0100

    created model class

--- headers/model.h
+++ headers/model.h
@@ -16,30 +16,21 @@
 
 class Model {
 public:
-    // 1. The constructor loads the file
     Model(const char* filePath);
 
-    // 2. The Draw method calls Draw on all its meshes
     void draw(Shader& shader, camera& cam);
 
 private:
     std::vector<mesh> meshes;
-    std::string directory; // To find textures relative to the OBJ file location
+    std::string directory;
 
-    // --- Assimp Internal Methods ---
     void loadModel(const std::string& path);
 
-    // Handles the hierarchy (bones/parts) of the model file
     void processNode(aiNode* node, const aiScene* scene);
 
-    // Converts an Assimp mesh into *your* mesh object
     mesh processMesh(aiMesh* aiMesh, const aiScene* scene);
 
-    // Loads textures from the material data
     std::vector<texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
-
-    // (Optional but helpful) A simple function to load textures you define
-    unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma = false);
 };
 
 #endif //OPENGLPROJECT_MODEL_H
